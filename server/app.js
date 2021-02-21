8849fdc45a555e0db99c952d8808fc9393b57f45
IgalRach <iga_rahman@yahoo.com>
2021-02-21 16:56:54 +0200

    added web socket and socket.io to the project

--- server/app.js
+++ server/app.js
@@ -1,27 +1,45 @@
-const  express = require('express');
+const express = require('express')();
+var http = require('http').createServer(express);
+var io = require('socket.io')(http);
+
+
 //--------------------------------Database Connection--------------------------------------------------
 const mongoose = require('mongoose');
 const connectionString = ("mongodb+srv://easycook:asd123@easycook.8bkru.mongodb.net/<easycook>?retryWrites=true&w=majority");
 
-mongoose.connect(connectionString,{useNewUrlParser: true,  useUnifiedTopology: true });
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 //-----------------------------------End of Database Connection----------------------------------------
-const recipes = require('./routes/recipes');
-const geners = require('./routes/geners');
-const accounts = require('./routes/accounts');
-const comments = require('./routes/comments');
-const bodyParser = require('body-parser');
-const app = express();
-app.use(bodyParser.urlencoded({extended:true}));
-app.use('/recipes',recipes);
-app.use('/accounts',accounts);
-app.use('/geners',geners);
-app.use('/comments',comments);
+// const recipes = require('./routes/recipes');
+// const geners = require('./routes/geners');
+// const accounts = require('./routes/accounts');
+// const comments = require('./routes/comments');
+// const bodyParser = require('body-parser');
+// const app = express();
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use('/recipes', recipes);
+// app.use('/accounts', accounts);
+// app.use('/geners', geners);
+// app.use('/comments', comments);
+
+express.get('/', (req, res) => {
+    res.sendFile(__dirname + '/p.html');
+});
+
+io.on('connection', (socket) => {
+    socket.broadcast.emit('joined','');
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('disconnect','');
+    });
+    
+    socket.on('whatever', (msg) => {
+        socket.emit('new message',msg)
+    });
+
+});
 
 
-
-
-
-app.listen(8082);
+http.listen(8082);
 
